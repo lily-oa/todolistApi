9651ba9f2f4c330e8d6e0e3f3e6aa8fd9045b3e0
lily <a0976198586@gmail.com>
2023-01-31 05:09:28 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,4 +2,6 @@ const apiUrl = 'https://todoo.5xcamp.us';
 const email = document.querySelector('#Email1');
 const password = document.querySelector('#Password1');
 const login_btn = document.querySelector('.login_btn');
+const alert_txt = document.querySelector('.alert_txt');
 const status_txt = document.querySelector('.status_txt');
+const modal = document.querySelector('#login_modal');
