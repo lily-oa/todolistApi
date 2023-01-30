4c68b641fc61825f1e69c01e3da41e2a71fd160b
lily <a0976198586@gmail.com>
2023-01-31 05:09:46 +0800

    Update 2023-01-30T21:09:44.505Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,5 +4,7 @@ var apiUrl = 'https://todoo.5xcamp.us';
 var email = document.querySelector('#Email1');
 var password = document.querySelector('#Password1');
 var login_btn = document.querySelector('.login_btn');
+var alert_txt = document.querySelector('.alert_txt');
 var status_txt = document.querySelector('.status_txt');
+var modal = document.querySelector('#login_modal');
 //# sourceMappingURL=all.js.map
