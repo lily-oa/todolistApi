8b24ca5bd27f815759462b6d14fb0d2969700fbf
lily <a0976198586@gmail.com>
2023-01-30 22:25:49 +0800

    Update 2023-01-30T14:25:47.926Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,2 +1,7 @@
 "use strict";
+
+var apiUrl = 'https://todoo.5xcamp.us';
+var email = document.querySelector('#Email1');
+ocument.querySelector('#email1');
+var password = document.querySelector('#Password1');
 //# sourceMappingURL=all.js.map
