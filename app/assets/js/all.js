c98ac21f4d7b39d75a103700ea9dde6901c62cba
lily <a0976198586@gmail.com>
2023-02-01 07:29:58 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -9,4 +9,33 @@ const modal = document.querySelector('#login_modal');
 const loginModal = new bootstrap.Modal(modal, {})
 
 
+const input = async
 
+function check(){
+  let isnull = false;
+  for(const item of allinput){
+    if(item.value === ''){
+      isnull = true;
+      break;
+    }
+  }
+  if (isnull === true){
+    alert_txt.textContent = '您還有欄位尚未填寫喔!!';
+    loginModal.show();
+    reset();
+    return;
+  }
+  //email的輸入值字串必須有 @
+  if(email.value.match('@') === null){
+    alert.text.textContent = '您的Email格式不正確!!!';
+    loginModal.show();
+    reset();
+    return;
+  }
+  return true;
+}
+
+function reset(){
+  email.value = '';
+  password.value = '';
+}
