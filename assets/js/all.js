60f00fa2816e81b6cd9adf3e764bc27d6184cc79
lily <a0976198586@gmail.com>
2023-01-31 12:46:16 +0800

    Update 2023-01-31T04:46:14.547Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,10 +1,12 @@
 "use strict";
 
 var apiUrl = 'https://todoo.5xcamp.us';
+var allinput = document.querySelectorAll('input');
 var email = document.querySelector('#Email1');
 var password = document.querySelector('#Password1');
 var login_btn = document.querySelector('.login_btn');
 var alert_txt = document.querySelector('.alert_txt');
 var status_txt = document.querySelector('.status_txt');
 var modal = document.querySelector('#login_modal');
+var loginModal = new bootstrap.Modal(modal, {});
 //# sourceMappingURL=all.js.map
