065899a38543ff536fb56668e8ab814a82628967
lily <a0976198586@gmail.com>
2023-02-03 23:47:36 +0800

    Update 2023-02-03T15:47:35.100Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -114,4 +114,148 @@ function reset() {
   email.value = '';
   password.value = '';
 }
+
+//---------------------------------------------------------------signUp JS
+
+//共用 apiUrl、 allinput
+
+// const mail = document.querySelector('#signUpEmail');
+// const nickname = document.querySelector('#signUpNickName');
+// const first_pwd = document.querySelector('#signUpPassword');
+// const confirm_pwd = document.querySelector('#signUpPassword2');
+// const sign_btn = document.querySelector('.signup_btn');
+// const signup_alert_txt = document.querySelector('.signup_alert_txt');
+// const signup_modal = document.querySelector('.signup_modal');
+// const signup_status_txt = document.querySelector('.signup_status_txt');
+// const myModal = new bootstrap.Modal(modal, {})
+
+// signup_reset();
+
+// sign_btn.addEventListener('click', () =>{
+//   const format_isok = signup_check();
+//   if(format_isok === true){
+//     signup(mail, nickname, password)
+//   }else{
+//     return
+//   }
+//   console.log(132);
+// })
+
+// function signup_check(){
+//   let isnull = false;
+
+//   for(const item of allinput){
+//     if(item.value === ''){
+//       isnull = true;
+//       break;
+//     }
+//   }
+//   if(isnull === true){
+//     signup_alert_txt.textContent = '您還有欄位尚未填寫';
+//     myModal.show();
+//   }
+//   if(signUpEmail.value.match('@') === null){
+//     signup_alert_txt.textContent = 'Email 格式不正確';
+//     myModal.show();
+//     signup_reset();
+//     return;
+//   }
+//   if(first_pwd.value.trim().length < 6){
+//     signup_alert_txt.textContent = '密碼必須6個字以上喔 ! ';
+//     myModal.show();
+//     first_pwd.value = '';
+//     confirm_pwd.value = '';
+//     return;
+//   }
+//   if(first_pwd.value !== confirm_pwd.value){
+//     signup_alert_txt.textContent = '兩次的密碼輸入不一致喔 ! ';
+//     myModal.show();
+//     first_pwd.value = '';
+//     confirm_pwd.value = '';
+//     return;
+//   }
+//   return true;
+// }
+
+// function signup_reset(){
+//   mail.value = '';
+//   nickname.value = '';
+//   first_pwd.value = '';
+//   confirm_pwd.value = '';
+// }
+"use strict";
+
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
+var apiUrl = 'https://todoo.5xcamp.us';
+var allinput = document.querySelectorAll('input');
+var mail = document.querySelector('#signUpEmail');
+var nickname = document.querySelector('#signUpNickName');
+var first_pwd = document.querySelector('#signUpPassword');
+var confirm_pwd = document.querySelector('#signUpPassword2');
+var sign_btn = document.querySelector('.signup_btn');
+var signup_alert_txt = document.querySelector('.signup_alert_txt');
+var signup_modal = document.querySelector('.signup_modal');
+var signup_status_txt = document.querySelector('.signup_status_txt');
+var myModal = new bootstrap.Modal(modal, {});
+signup_reset();
+sign_btn.addEventListener('click', function () {
+  var format_isok = signup_check();
+  if (format_isok === true) {
+    signup(mail, nickname, password);
+  } else {
+    return;
+  }
+  console.log(132);
+});
+function signup_check() {
+  var isnull = false;
+  var _iterator = _createForOfIteratorHelper(allinput),
+    _step;
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var item = _step.value;
+      if (item.value === '') {
+        isnull = true;
+        break;
+      }
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+  if (isnull === true) {
+    signup_alert_txt.textContent = '您還有欄位尚未填寫';
+    myModal.show();
+  }
+  if (signUpEmail.value.match('@') === null) {
+    signup_alert_txt.textContent = 'Email 格式不正確';
+    myModal.show();
+    signup_reset();
+    return;
+  }
+  if (first_pwd.value.trim().length < 6) {
+    signup_alert_txt.textContent = '密碼必須6個字以上喔 ! ';
+    myModal.show();
+    first_pwd.value = '';
+    confirm_pwd.value = '';
+    return;
+  }
+  if (first_pwd.value !== confirm_pwd.value) {
+    signup_alert_txt.textContent = '兩次的密碼輸入不一致喔 ! ';
+    myModal.show();
+    first_pwd.value = '';
+    confirm_pwd.value = '';
+    return;
+  }
+  return true;
+}
+function signup_reset() {
+  mail.value = '';
+  nickname.value = '';
+  first_pwd.value = '';
+  confirm_pwd.value = '';
+}
 //# sourceMappingURL=all.js.map
