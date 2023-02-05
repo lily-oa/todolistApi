9a48f39039b50f947c0c9745b9379cb19778940b
lily <a0976198586@gmail.com>
2023-02-05 16:15:52 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -29,32 +29,63 @@ function login(email, password) {
   )
 }
 
-const input = async(mail, pwd) => {
-  try{
-    const res = await login(mail, pwd);
-    //設置默認標頭的機制語法，該標頭將隨您發出的每個請求一起發送
-    axios.defaults.headers.common['Authorization'] = res.headers.Authorization
-    sessionStorage.setItem('token', res.headers.Authorization)
-    sessionStorage.setItem('name', res.data.nickname)
-    setTimeout(() =>{
-      status_txt.textContent = '';
-      alert_txt.innerHTML = `登入成功 ! 歡迎${res.data.nickname} 回來 <br><br> 即將跳轉待辦清單...`;
-      loginModal.show();
-      reset();
-      setTimeout(() =>{
-        document.location.href='./addTodos.html';
-      }, 2000);
-    }, 1000);
+// const input = async(mail, pwd) => {
+//   try{
+//     const res = await login(mail, pwd);
+//     //設置默認標頭的機制語法，該標頭將隨您發出的每個請求一起發送
+//     axios.defaults.headers.common['Authorization'] = res.headers.Authorization
+//     sessionStorage.setItem('token', res.headers.Authorization)
+//     sessionStorage.setItem('name', res.data.nickname)
+//     setTimeout(() =>{
+//       status_txt.textContent = '';
+//       alert_txt.innerHTML = `登入成功 ! 歡迎${res.data.nickname} 回來 <br><br> 即將跳轉待辦清單...`;
+//       loginModal.show();
+//       reset();
+//       setTimeout(() =>{
+//         document.location.href='./addTodos.html';
+//       }, 2000);
+//     }, 1000);
 
-  }catch(error){
-    setTimeout(() =>{
-      status_txt.textContent = '';
-      alert_txt.textContent = '登入失敗，您的Email或密碼有誤!'
-      loginModal.show()
-      reset();
-    }, 1000);
+//   }catch(error){
+//     setTimeout(() =>{
+//       status_txt.textContent = '';
+//       alert_txt.textContent = '登入失敗，您的Email或密碼有誤!'
+//       loginModal.show()
+//       reset();
+//     }, 1000);
+//   }
+// }
+
+const input = async (mail, pwd) => {
+  try {
+      const res = await login(mail, pwd);
+      axios.defaults.headers.common['Authorization'] = res.headers.authorization
+      sessionStorage.setItem('token', res.headers.authorization)
+      sessionStorage.setItem('name', res.data.nickname)
+      setTimeout(() => {
+          status_txt.textContent = '';
+          alert_txt.innerHTML = `登入成功 ! 歡迎 ${res.data.nickname} 回來 <br><br> 即將跳轉待辦清單 ...`;
+          loginModal.show()
+          reset();
+          setTimeout(() => {
+              document.location.href = './list.html';
+          }, 2000);
+      }, 1000);
+
+  } catch (error) {
+      setTimeout(() => {
+          status_txt.textContent = '';
+          alert_txt.textContent = '登入失敗，您的Email或密碼有誤 !  '
+          loginModal.show()
+          reset();
+      }, 1000);
+
   }
 }
+
+
+
+
 
 function check(){
   let isnull = false;
@@ -84,72 +115,3 @@ function reset(){
   email.value = '';
   password.value = '';
 }
-
-//---------------------------------------------------------------signUp JS
-
-//共用 apiUrl、 allinput
-
-// const mail = document.querySelector('#signUpEmail');
-// const nickname = document.querySelector('#signUpNickName');
-// const first_pwd = document.querySelector('#signUpPassword');
-// const confirm_pwd = document.querySelector('#signUpPassword2');
-// const sign_btn = document.querySelector('.signup_btn');
-// const signup_alert_txt = document.querySelector('.signup_alert_txt');
-// const signup_modal = document.querySelector('.signup_modal');
-// const signup_status_txt = document.querySelector('.signup_status_txt');
-// const myModal = new bootstrap.Modal(modal, {})
-
-// signup_reset();
-
-// sign_btn.addEventListener('click', () =>{
-//   const format_isok = signup_check();
-//   if(format_isok === true){
-//     signup(mail, nickname, password)
-//   }else{
-//     return
-//   }
-//   console.log(132);
-// })
-
-// function signup_check(){
-//   let isnull = false;
-
-//   for(const item of allinput){
-//     if(item.value === ''){
-//       isnull = true;
-//       break;
-//     }
-//   }
-//   if(isnull === true){
-//     signup_alert_txt.textContent = '您還有欄位尚未填寫';
-//     myModal.show();
-//   }
-//   if(signUpEmail.value.match('@') === null){
-//     signup_alert_txt.textContent = 'Email 格式不正確';
-//     myModal.show();
-//     signup_reset();
-//     return;
-//   }
-//   if(first_pwd.value.trim().length < 6){
-//     signup_alert_txt.textContent = '密碼必須6個字以上喔 ! ';
-//     myModal.show();
-//     first_pwd.value = '';
-//     confirm_pwd.value = '';
-//     return;
-//   }
-//   if(first_pwd.value !== confirm_pwd.value){
-//     signup_alert_txt.textContent = '兩次的密碼輸入不一致喔 ! ';
-//     myModal.show();
-//     first_pwd.value = '';
-//     confirm_pwd.value = '';
-//     return;
-//   }
-//   return true;
-// }
-
-// function signup_reset(){
-//   mail.value = '';
-//   nickname.value = '';
-//   first_pwd.value = '';
-//   confirm_pwd.value = '';
-// }
