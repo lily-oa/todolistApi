ed368cdd2e7103c4e834e8fe989290807ebbe386
lily <a0976198586@gmail.com>
2023-02-06 10:17:00 +0800

    Update 2023-02-06T02:16:58.830Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -119,13 +119,13 @@ function reset() {
 function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
 function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
-//const apiUrl = 'https://todoo.5xcamp.us';
-//onst mail = document.querySelector('#Email1');
+var apiUrl = 'https://todoo.5xcamp.us';
+var mail = document.querySelector('#Email1');
 var nickname = document.querySelector('#nickname');
-//const password = document.querySelector('#Password1');
+var password = document.querySelector('#Password1');
 var confirm_pwd = document.querySelector('#Password2');
 var sign_btn = document.querySelector('.signup_btn');
-//const allinput = document.querySelectorAll('input');
+var allinput = document.querySelectorAll('input');
 var signup_alert_txt = document.querySelector('.signup_alert_txt');
 var signup_modal = document.querySelector('.signup_modal');
 var signup_status_txt = document.querySelector('.signup_status_txt');
