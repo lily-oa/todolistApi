027615f316725f5aa25f5925c294e08bea46d2ef
lily <a0976198586@gmail.com>
2023-02-08 23:02:45 +0800

    Update 2023-02-08T15:02:43.700Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -27,6 +27,7 @@ var signUpBtn = document.querySelector('.signup_btn');
 var signup_alert_txt = document.querySelector('.signup_alert_txt');
 var signup_status_txt = document.querySelector('.signup_status_txt');
 
+//將modal的動元素名稱設為一樣 
 //const signup_modal = document.querySelector('.signup_modal');
 //const signup_myModal = new bootstrap.Modal(signup_modal, {})
 
