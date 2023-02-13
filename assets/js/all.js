4d8a14befaa54dd5c24ae0d677c2fc86714032b6
lily <a0976198586@gmail.com>
2023-02-13 09:00:16 +0800

    Update 2023-02-13T01:00:14.584Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -15,17 +15,8 @@ var loginPassword = document.querySelector('#login-password');
 var login_btn = document.querySelector('.login_btn');
 var login_alert_txt = document.querySelector('.login_alert_txt');
 var login_status_txt = document.querySelector('.login_status_txt');
-var callModal = document.querySelector('#show_modal');
-var showModal = new bootstrap.Modal(callModal, {});
-//signup---------------------------------------
-var signUpInput = document.querySelector('.signUpInput');
-var signUpEmail = document.querySelector('#signUpEmail');
-var signUpNickname = document.querySelector('#signUpNickname');
-var signUpPassword = document.querySelector('#signUpPassword');
-var signUpPassword2 = document.querySelector('#signUpPassword2');
-var signUpBtn = document.querySelector('.signup_btn');
-var signup_alert_txt = document.querySelector('.signup_alert_txt');
-var signup_status_txt = document.querySelector('.signup_status_txt');
+// const callModal = document.querySelector('#show_modal');
+// const showModal = new bootstrap.Modal(callModal, {})
 
 //將modal的動元素名稱設為一樣 
 //const signup_modal = document.querySelector('.signup_modal');
@@ -130,8 +121,22 @@ function loginReset() {
   loginEmail.value = '';
   loginPassword.value = '';
 }
+"use strict";
 
-//signup 設定------------------------------
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
+//signup---------------------------------------
+var signUpInput = document.querySelector('.signUpInput');
+var signUpEmail = document.querySelector('#signUpEmail');
+var signUpNickname = document.querySelector('#signUpNickname');
+var signUpPassword = document.querySelector('#signUpPassword');
+var signUpPassword2 = document.querySelector('#signUpPassword2');
+var signUpBtn = document.querySelector('.signup_btn');
+var signup_alert_txt = document.querySelector('.signup_alert_txt');
+var signup_status_txt = document.querySelector('.signup_status_txt');
+
+//-----------------------------------
 
 signupReset();
 signUpBtn.addEventListener('click', function () {
@@ -171,20 +176,20 @@ function signup(signUpEmail, signUpNickname, signUpPassword) {
 }
 function signupCheck() {
   var isnull = false;
-  var _iterator2 = _createForOfIteratorHelper(signUpInput),
-    _step2;
+  var _iterator = _createForOfIteratorHelper(signUpInput),
+    _step;
   try {
-    for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
-      var item = _step2.value;
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var item = _step.value;
       if (item.value == '') {
         isnull = true;
         break;
       }
     }
   } catch (err) {
-    _iterator2.e(err);
+    _iterator.e(err);
   } finally {
-    _iterator2.f();
+    _iterator.f();
   }
   if (isnull === true) {
     signup_alert_txt.textContent = '您還有欄位尚未填寫';
