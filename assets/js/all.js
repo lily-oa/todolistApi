cc32dd025093e561c8987defa1af315aecf39c2c
lily <a0976198586@gmail.com>
2023-03-12 08:13:51 +0800

    Update 2023-03-12T00:13:49.776Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -216,6 +216,7 @@ function signupCheck() {
   if (signUpPassword.value.trim().length < 6) {
     signup_alert_txt.textContent = '密碼必須6個字以上喔 ! ';
     signupModal.show();
+    // 為何以下這兩個程式碼不能用 ignupReset(); 去取代 
     signUpPassword.value = '';
     signUpPassword2.value = '';
     return;
@@ -223,6 +224,7 @@ function signupCheck() {
   if (signUpPassword.value !== signUpPassword2.value) {
     signup_alert_txt.textContent = '兩次的密碼輸入不一致喔 ! ';
     signupModal.show();
+    // 為何以下這兩個程式碼不能用 ignupReset(); 去取代 
     signUpPassword.value = '';
     signUpPassword2.value = '';
     return;
