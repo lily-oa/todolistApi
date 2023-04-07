062e414b218e73cb7136ae51bd55431149648ef2
lily <a0976198586@gmail.com>
2023-04-07 20:23:06 +0800

    update

--- app/assets/js/addTodos.js
+++ app/assets/js/addTodos.js
@@ -10,14 +10,17 @@ let logout_btn;
 // 檢查有無token，若無token 不顯示登入後畫面
 init_token_render();
 
-function init_token_render(){
-  if(sessionStorage.getItem('token')){
+function init_token_render() {
+  if (sessionStorage.getItem('token')) {
     const user_name = sessionStorage.getItem('name')
     header_logout.innerHTML = `
     <span class="d-none d-lg-block me-7 username">${user_name}的代辦事項</span>
     <a href="#" class="text-dark fs-7 fs-lg-6 logoutBtn">登出</a>
     `
-  }else{
-    
+    logout_btn = document.querySelector('.logoutBtn');
+  } else {
+    header_logout.innerHTML = `
+      
+  `
   }
 }
