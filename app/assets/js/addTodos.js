bd5fa3ca4b48aecc77975241ec1a05eb307327b4
lily <a0976198586@gmail.com>
2023-04-07 20:10:20 +0800

    update

--- app/assets/js/addTodos.js
+++ app/assets/js/addTodos.js
@@ -1,6 +1,6 @@
 const user = document.querySelector('.username');
 const header_logout = document.querySelector('.header_logout');
-
+let logout_btn;
 
 
 
@@ -12,10 +12,12 @@ init_token_render();
 
 function init_token_render(){
   if(sessionStorage.getItem('token')){
-    let user_name = sessionStorage.getItem('name')
+    const user_name = sessionStorage.getItem('name')
     header_logout.innerHTML = `
-    <span class="d-none d-lg-block me-7 username">${user}的代辦事項</span>
-    <a href="" class="text-dark fs-7 fs-lg-6 logoutBtn">登出</a>
+    <span class="d-none d-lg-block me-7 username">${user_name}的代辦事項</span>
+    <a href="#" class="text-dark fs-7 fs-lg-6 logoutBtn">登出</a>
     `
+  }else{
+    
   }
 }
