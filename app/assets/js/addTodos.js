3a32e7e3d4bfbb04860b03b687b8f27887e9641d
lily <a0976198586@gmail.com>
2023-04-10 15:58:18 +0800

    update

--- app/assets/js/addTodos.js
+++ app/assets/js/addTodos.js
@@ -2,9 +2,6 @@ const user = document.querySelector('.username');
 const header_logout = document.querySelector('.header_logout');
 
 
-
-
-
 // init start
 
 // 檢查有無token，若無token 不顯示登入後畫面
@@ -20,10 +17,29 @@ const header_logout = document.querySelector('.header_logout');
     <a href="#" class="text-dark fs-7 fs-lg-6 logoutBtn">登出</a>
     `
       } else {
-        document.location.href="index.html";
+        return;
       }
     }
   }
 
 // 登出
-const logoutBtn = document.querySelector('.log')
+const logoutBtn = document.querySelector('.logoutBtn');
+if (logoutBtn) {
+  logoutBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    axios.delete(`${apiUrl}/users/sign_out`
+    )
+  })
+  .then((res) => {
+    Swal.fire(
+      `${res.data.message}`,
+      "已登出!",
+      "success"
+    ).then((result) => {
+      if(result.isConfirmed){
+        window.location.assign("index.html");
+      }
+    })
+    .catch((err) => console.log(err.response));
+  })
+}
