e63d38aaa31103d06f47c6e131c91d1451b3a568
lily <a0976198586@gmail.com>
2023-04-12 12:56:48 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -7,7 +7,7 @@ const loginEmail = document.querySelector('#login-email');
 const loginPassword = document.querySelector('#login-password');
 const login_alert_txt = document.querySelector('.login_alert_txt');  //在 loginModal 上
 const login_status_txt = document.querySelector('.login_status_txt');
-const header_logout = document.querySelector('.header_logout');
+
 
 
 //login 設定------------------------------
