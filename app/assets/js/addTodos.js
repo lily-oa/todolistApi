f2154fcb6dd20d1a034181cbe9e00fb3b0929a00
lily <a0976198586@gmail.com>
2023-04-13 20:00:04 +0800

    update

--- app/assets/js/addTodos.js
+++ app/assets/js/addTodos.js
@@ -1,14 +1,19 @@
 const user = document.querySelector('.username');
 const header_logout = document.querySelector('.header_logout');
+const nonList = document.querySelector('.none-list');
+const listBlock = document.querySelector('.list-block');
 
 
-// init start
-// 檢查有無token，若無token 不顯示登入後畫面
 
-    if(header_logout){
+
+
+// 登入成功後呈現使用者名稱
+// 檢查.header_logout 是否有值，若有才執行init_token_render
+    if (header_logout) {
       init_token_render();
     }
-    
+
+// 檢查有無token，若無token 不顯示登入後畫面
     function init_token_render() {
       if (sessionStorage.getItem('token')) {
         const user_name = sessionStorage.getItem('name')
@@ -20,30 +25,29 @@ const header_logout = document.querySelector('.header_logout');
         return;
       }
     }
-  
 
-
-// 登出
-const logoutBtn = document.querySelector('.logoutBtn');
-if (logoutBtn) {
-  logoutBtn.addEventListener('click', function (e) {
-    e.preventDefault();
-    axios.delete(`${apiUrl}/users/sign_out`, {
-        headers: {
-          Authorization: sessionStorage.token,
-        },
-      })
-      .then((res) => {
-        Swal.fire(
-            `${res.data.message}`,
-            "已登出!",
-            "success"
-          ).then((result) => {
-            if (result.isConfirmed) {
-              window.location.assign("index.html");
-            }
+    // 登出
+    const logoutBtn = document.querySelector('.logoutBtn');
+    if (logoutBtn) {
+      logoutBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        axios.delete(`${apiUrl}/users/sign_out`, {
+            headers: {
+              Authorization: sessionStorage.token,
+            },
           })
-          .catch((err) => console.log(err.response));
+          .then((res) => {
+            Swal.fire(
+                `${res.data.message}`,
+                "已登出!",
+                "success"
+              ).then((result) => {
+                if (result.isConfirmed) {
+                  window.location.assign("index.html");
+                }
+              })
+              .catch((err) => console.log(err.response));
+          })
       })
-  })
-}
+    }
+
