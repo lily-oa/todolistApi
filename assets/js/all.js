b1d83af2bd7fc55092ce7e31771a4e94f863a15d
lily <a0976198586@gmail.com>
2023-04-19 20:39:22 +0800

    Update 2023-04-19T12:39:20.510Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -69,6 +69,10 @@ function removeAll() {
 }
 
 //新增代碼
+if (enterBtn) {
+  enterBtn.addEventListener('click', addTodo);
+}
+function addTodo() {}
 "use strict";
 
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
