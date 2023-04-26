772e0a2c8c558597f501ed9473ed452c21c84e15
lily <a0976198586@gmail.com>
2023-04-26 13:04:54 +0800

    update

--- app/assets/js/addTodos.js
+++ app/assets/js/addTodos.js
@@ -60,30 +60,30 @@ let data = [];
 
 
 //渲染畫面
-// function renderData(arr) {
-//   let str = '';
-//   arr.forEach((item) => {
-//     str += `<li data-id="${item.id}">
-//     <label for="" class="checkbox">
-//       <input type="checkbox" class="form-check-input">
-//       <span class="ps-4">${item.content}</span>
-//     </label>
-//     <a href="#" class="delete"></a>
-//   </li>`;
-//   });
-//   nonList.setAttribute("class", "d-none");
-//   listBlock.setAttribute('class', 'd-block');
-//   list.innerHtml = str;
-//   removeAll();
-// }
+function renderData(arr) {
+  let str = '';
+  arr.forEach((item) => {
+    str += `<li data-id="${item.id}">
+    <label for="" class="checkbox">
+      <input type="checkbox" class="form-check-input">
+      <span class="ps-4">${item.content}</span>
+    </label>
+    <a href="#" class="delete"></a>
+  </li>`;
+  });
+  nonList.setAttribute("class", "d-none");
+  listBlock.setAttribute('class', 'd-block');
+  list.innerHtml = str;
+  removeAll();
+}
 
 // clear All
-// function removeAll() {
-//   if (data.length === 0) {
-//     listBlock.setAttribute('class', 'd-none');
-//     nonList.removeAttribute('class', 'd-none');
-//   }
-// }
+function removeAll() {
+  if (data.length === 0) {
+    listBlock.setAttribute('class', 'd-none');
+    nonList.removeAttribute('class', 'd-none');
+  }
+}
 
 function getTodo() {
   return axios.get(`${apiUrl}/todos`, {
@@ -136,52 +136,52 @@ function addTodo(e) {
       obj.check = '';
       data.unshift(obj);
       console.log(inputText.value);
-      //inputText.value = '';
-      //updateList();
+      inputText.value = '';
+      updateList();
     })
     .catch((err) => console.log(err.response));
 }
 
 // 按鈕輸入
-// if(inputBlock){
-//   inputBlock.addEventListener('keydown', function(e){
-//     if(e.key === 'Enter'){
-//       addTodo();
-//     }
-//   })
-// }
+if(inputBlock){
+  inputBlock.addEventListener('keydown', function(e){
+    if(e.key === 'Enter'){
+      addTodo();
+    }
+  })
+}
 
-// 切換畫面
-// const tab = document.querySelector('.tab');
-// let tabStatus = 'all';
-// if(tab){
-//   tab.addEventListener('click', function(e){
-//     tabStatus = e.target.dataset.status;
-//     let tabs = document.querySelectorAll('.tab li');
-//     tabs.forEach((i) => {
-//       i.classList.remove('tabs-active');
-//     });
-//     e.target.classList.add('tabs-active');
-//     updateList();
-//   });
-// }
+//切換畫面
+const tab = document.querySelector('.tab');
+let tabStatus = 'all';
+if(tab){
+  tab.addEventListener('click', function(e){
+    tabStatus = e.target.dataset.status;
+    let tabs = document.querySelectorAll('.tab li');
+    tabs.forEach((i) => {
+      i.classList.remove('tabs-active');
+    });
+    e.target.classList.add('tabs-active');
+    updateList();
+  });
+}
 
-// let undoNum = document.querySelector('.undo-num');
-// function updateList(){
-//   let showData = [];
+let undoNum = document.querySelector('.undo-num');
+function updateList(){
+  let showData = [];
 
-//   if(tabStatus === 'all'){
-//     showData = data;
-//   }else if(tabStatus === 'undo'){
-//     showData = data.filter((i) => i.completed_at === null);
-//   }else if(tabStatus === 'done'){
-//     showData = data.filter((i) => i.completed_at !== null);
-//   }
+  if(tabStatus === 'all'){
+    showData = data;
+  }else if(tabStatus === 'undo'){
+    showData = data.filter((i) => i.completed_at === null);
+  }else if(tabStatus === 'done'){
+    showData = data.filter((i) => i.completed_at !== null);
+  }
 
-//   let todoLength = data.filter((i) => i.completed_at === null);
-//   let str = `${todoLength.length} 個待完成項目`;
-//   undoNum.innerHTML = str;
+  let todoLength = data.filter((i) => i.completed_at === null);
+  let str = `${todoLength.length} 個待完成項目`;
+  undoNum.innerHTML = str;
 
-//   renderData(showData);
-// }
+  renderData(showData);
+}
 
