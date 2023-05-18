69ea4618b5be4eab45f3446e116e49230ff565d3
lily <a0976198586@gmail.com>
2023-05-18 16:37:03 +0800

    update

--- app/assets/js/addTodos.js
+++ app/assets/js/addTodos.js
@@ -299,8 +299,7 @@ if(clearAll){
         //-------------------燈箱效果
         addTodosModal.show();
         setTimeout(() => {
-          addTodos_alert_txt.innerHTML = `全部刪除成功!${res.data}`;
-          
+          addTodos_alert_txt.innerHTML = `全部刪除成功!`;
         },2000)
         //--------------------
       })
